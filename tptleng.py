d7e6999c794e5cee745690dd15525099cda02fb0
natalia <natalia.pesaresi@gmail.com>
2018-07-05 14:00:28 -0300

    avance

--- tptleng.py
+++ tptleng.py
@@ -17,18 +17,10 @@ t_NULL = r'null'
 
 def t_NUMBER(t):
     r'-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?'
-    try:
-        t.value = float(t.value)
-    except ValueError:
-        print("Integer value too large %d", t.value)
-        t.value = 0
-    #t.tipo = "num"
     return t
 
 def t_STRING(t):
     r'\"[^\\"]*\"'
-    t.tipo = "string"
-    t.value=t.value
     return t
 
 t_ignore = " \t"
@@ -42,117 +34,94 @@ def t_error(t):
 
 lexer = lex.lex()
 
+class TokenWithAttributes:
+	def __init__(self, tipo, listaTokensWithAttribute, yaml):
+		self.tipo = tipo
+		self.listaTokensWithAttribute = listaTokensWithAttribute
+		self.yaml = yaml
+
 start = 'first'
 
 def p_first(p):
-    'first : value'
-    p[0].tipo="primero"
-    p[1].tabs = 0
-    p[0].yaml = "" + p[1].yaml
-    print(p[0].yaml)
+	'first : value'
+	parsingTree = TokenWithAttribute("primero", p[1], "")
+	p[0] = p[1].imprimir();
 
 def p_object(p):
     ''' object : LLAVEIZQ LLAVEDER
                 | LLAVEIZQ members LLAVEDER '''
 
-    p[0].tipo = "object"
     if p[2] == '}':
         # O -> { }
-        p[0].yaml="{}"
+        p[0] = TokenWithAttributes("terminal",[],"{}")
     else:
         # O -> { M }
-        p[2].tabs = p[0].tabs+1
-        p[0].yaml += "\n" + p[2].yaml
+        p[0] = TokenWithAttributes("object",[p[1]],"")
 
 def p_members(p):
     '''members : pair
                 | pair COMA members '''
 
-    p[0].tipo="lista"
-    p[1].tabs = p[0].tabs
-    p[0].yaml += "\n"+p[1].yaml + "\n"
-    # E -> V
-
-    if len(p) > 2:
-        # E -> V, E
-        p[3].tabs = p[0].tabs
-        p[0].yaml += p[3].yaml
+	if (len(p) > 2):
+		# E -> V, E
+	        p[0] = TokenWithAttributes("lista",[p[1],p[3]],"")
+	else:
+		# E -> V
+	        p[0] = TokenWithAttributes("lista",[p[1]],"")
 
 def p_pair(p):
     'pair : STRING DOSPUNTOS value'
     # P -> string : M
-    p[0].tipo = "par"
-    p[3].tabs = p[0].tabs+1
-    p[0].yaml = p[0].tabs*"\t" + p[1].value+": "+p[3].yaml
+        p[0] = TokenWithAttributes("par",[p[1],p[3]],"")
 
 def p_array(p):
     '''array : CORCHEIZQ CORCHEDER
             | CORCHEIZQ elements CORCHEDER'''
 
-    p[0].tipo = "array"
     if len(p) == 3:
         # A -> [ ]
-        p[0].yaml = "[]"
+        p[0] = TokenWithAttributes("terminal",[],"[]")
     else:
         # A -> [ E ]
-        p[2].tabs = p[0].tabs+1
-        p[0].yaml += "\n" + p[2].yaml
-
+        p[0] = TokenWithAttributes("array",[p[2]],"")
 
 def p_elements(p):
     ''' elements : value COMA elements
                 | value '''
 
-    p[0].tipo="lista"
     # E -> V
-    if(p[1].tipo in Set(["array", "object"])):
-        p[1].tabs = p[0].tabs+1
-        p[0].yaml += p[0].tabs*"\t"+"-"+"\n"+ p[1].yaml + "\n"
+    if(len(p) > 2):
+        p[0] = TokenWithAttributes("lista",[p[1],p[3]],"")
     else:
-        p[0].yaml += p[0].tabs*"\t"+"-"+ p[1].yaml + "\n"
-
-    if len(p) > 2:
-        # E -> V, E
-        p[3].tabs = p[0].tabs
-        p[0].yaml += p[3].yaml
-
-
+        p[0] = TokenWithAttributes("lista",[p[1]],"")
 
 def p_value_array(p):
     'value : array '
-    p[0].tipo = "array"
-    p[0].yaml = p[1].yaml
+        p[0] = TokenWithAttributes("array",[p[1]],"")
 
 def p_value_object(p):
     'value : object '
-    p[0].tipo = "object"
-    p[0].yaml = p[1].yaml
+        p[0] = TokenWithAttributes("object",[p[1]],"")
 
 def p_value_string(p):
     'value : STRING '
-    p[0].tipo = "valor"
-    p[0].yaml = p[1].value
+        p[0] = TokenWithAttributes("terminal",[],"string")
 
 def p_value_number(p):
     'value : NUMBER '
-    p[0].tipo = "valor"
-    p[0].yaml = p[1].value
+        p[0] = TokenWithAttributes("terminal",[],"number")
 
 def p_value_true(p):
     'value : TRUE '
-    p[0].tipo = "valor"
-    p[0].yaml = "true"
+        p[0] = TokenWithAttributes("terminal",[],"true")
 
 def p_value_false(p):
     'value : FALSE '
-    p[0].tipo = "valor"
-    p[0].yaml = "false"
+        p[0] = TokenWithAttributes("terminal",[],"false")
 
 def p_value_null(p):
     'value : NULL '
-    p[0].tipo = "valor"
-    p[0].yaml = ""
-
+        p[0] = TokenWithAttributes("terminal",[],"")
 
 
 def p_error(p):
@@ -169,3 +138,4 @@ lexer.input(f.read())
 parser = yacc.yacc(start = 'first')
 
 result = parser.parse(lexer = lexer)
+
